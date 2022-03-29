a2bc515b73631ec447c92550ac76e03a5b230d4e
Jay Paul Tucker <jaypaultucker@gmail.com>
2022-03-28 21:00:08 -0400

    Calculator Functions
    - Functionality for Adding Paychecks together

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
 function tester() {
-    console.log("hi")
+
+    // First Paycheck Avg Calculator
+    let paycheck1 = Number(document.getElementById("paycheck-1").value)
+    let paycheck2 = Number(document.getElementById("paycheck-2").value)
+
+    // Test Console Log to make sure Math Works
+    // console.log(paycheck1 + paycheck2)
+
+    let monthlyIncome = paycheck1 + paycheck2;
+
+    // Uses our monthlyIncome P tag by the ID and prints our Variable onto the page
+    document.getElementById("monthlyIncome").innerHTML = monthlyIncome;
 };
