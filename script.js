2d9cb9c8e29bf6c08dc058e481a24b100ec7caf9
Jay Paul Tucker <jaypaultucker@gmail.com>
2022-03-30 16:28:01 -0400

    Updated script.js
    - Added error for putting in letters instead of numbers for paycheck entry.
    - Added coding notes to script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
 function tester() {
-    console.log("hi")
+    // First Paycheck Avg Calculator
+    let paycheck1 = Number(document.getElementById("paycheck-1").value)
+    let paycheck2 = Number(document.getElementById("paycheck-2").value)
+
+
+    // Code below checks to see if there's a valid number entered into the text box.
+    if (Number.isFinite(paycheck1) && Number.isFinite(paycheck2)) {
+        console.log("This is a number")
+    } else {
+        console.log("This is NOT a number")
+        alert("The values entered below are not valid numbers, please try again")
+    }
+
+    // Test Console Log to make sure Math Works
+    // console.log(paycheck1 + paycheck2)
+
+    let monthlyIncome = paycheck1 + paycheck2;
+
+    // Uses our monthlyIncome P tag by the ID and prints our Variable onto the page
+    document.getElementById("monthlyIncome").innerHTML = monthlyIncome;
 };
