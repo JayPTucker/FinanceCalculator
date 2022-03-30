3cbe19855acf7c8d77876df94563dddd8b896e6b
Jay Paul Tucker <jaypaultucker@gmail.com>
2022-03-30 16:14:51 -0400

    Converting back to original Format
    Removed React temporarily

--- README.md
+++
@@ -1,2 +0,0 @@
-# FinanceCalculator
- A Calculator used to help determine your monthly income along with your available monthly finances.
--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-
 function tester() {
-
-    // First Paycheck Avg Calculator
-    let paycheck1 = Number(document.getElementById("paycheck-1").value)
-    let paycheck2 = Number(document.getElementById("paycheck-2").value)
-
-    // Test Console Log to make sure Math Works
-    // console.log(paycheck1 + paycheck2)
-
-    let monthlyIncome = paycheck1 + paycheck2;
-
-    // Uses our monthlyIncome P tag by the ID and prints our Variable onto the page
-    document.getElementById("monthlyIncome").innerHTML = monthlyIncome;
+    console.log("hi")
 };
