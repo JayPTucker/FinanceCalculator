3c3139e7a93979819691f7e0dc38fb15de740526
Jay Paul Tucker <jaypaultucker@gmail.com>
2022-03-30 17:18:26 -0400

    General Update
    - Added proper error messages for invalid integers in input field.
    - Error messages disappear once corrected after pressing Submit again
    - Added CSS
    - Edited the wording some using HTML

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ function tester() {
     let paycheck2 = Number(document.getElementById("paycheck-2").value)
 
 
-    // Code below checks to see if there's a valid number entered into the text box.
-    if (Number.isFinite(paycheck1) && Number.isFinite(paycheck2)) {
-        console.log("This is a number")
+    // The code below is designed to make sure there is a proper number (integer) entered into the input box
+    if (paycheck1 === 0) {
+        console.log("This is NOT a number (1)")
+        // alert("The values entered below are not valid numbers, please try again");
+        document.getElementById("error-Msg-1").innerHTML = "Error, Please type a proper integer above";
+    } else if (paycheck2 === 0) {
+        console.log("This is NOT a number (2)")
+        document.getElementById("error-Msg-2").innerHTML = "Error, Please type a proper integer above";
     } else {
-        console.log("This is NOT a number")
-        alert("The values entered below are not valid numbers, please try again")
+        document.getElementById("error-Msg-1").innerHTML = "";
+        document.getElementById("error-Msg-2").innerHTML = "";
+        console.log("This is a number");
     }
 
     // Test Console Log to make sure Math Works
