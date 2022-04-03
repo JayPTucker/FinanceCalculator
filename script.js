a3a7f3e2f6bd6d2500b849fae1a9ce73ee3749f9
Jay Paul Tucker <jaypaultucker@gmail.com>
2022-04-03 13:30:16 -0400

    LocalStorage Update
    - Saves all our data to LocalStorage so that when we return we can reuse said data.
    - Changed some HTML errors

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function isValidInput(paycheck, element) {
     }
 }
 
-
 function calculateIncome() {
     // Our Variables
 
@@ -19,18 +18,41 @@ function calculateIncome() {
     // Setting our Monthly income to the sum of our paychecks.
     let monthlyIncome = paycheck1 + paycheck2;
 
+
     // Checks using the function above to see if our paychecks are valid 
     paycheck1Valid = isValidInput(paycheck1, "error-Msg-1")
     paycheck2Valid = isValidInput(paycheck2, "error-Msg-2")
 
     // Verification for result
     if (paycheck1Valid === true && paycheck2Valid === true) {
-        document.getElementById("monthlyIncomeText").innerHTML = "Monthly Income:";
-        document.getElementById("monthlyIncome").innerHTML = "$" + monthlyIncome;    
+        document.getElementById("monthlyIncomeText").innerHTML = "New Monthly Income:";
+        document.getElementById("monthlyIncome").innerHTML = "$" + monthlyIncome + "/mo";  
+        // Add previous monthly income to local storage
+        // paycheck1
+        localStorage.setItem("paycheck-1", paycheck1);
+        // paycheck2
+        localStorage.setItem("paycheck-2", paycheck2);
+        // monthlyIncome
+        localStorage.setItem("monthly-income", monthlyIncome);
+        
     } else {
-        document.getElementById("monthlyIncomeText").innerHTML = "Unable to generate monthly income";
+        alert("Error, unable to generate monthly income");
         document.getElementById("monthlyIncome").innerHTML = "";    
+        onLoad()
     }
 
-    // // Uses our monthlyIncome P tag by the ID and prints our Variable onto the page
 };
+
+// Create a function that loads our last monthly income from local storage
+function onLoad() {
+
+    document.getElementById("monthlyIncomeText").innerHTML = "Last monthly income:"
+
+    document.getElementById("monthlyIncome").innerHTML = (
+        "Paycheck 1: $" + localStorage.getItem("paycheck-1") + "<br>" +
+        "Paycheck 2: $" + localStorage.getItem("paycheck-2") + "<br>" +
+        "Last monthly income: $" + localStorage.getItem("monthly-income") + "/mo"
+        );
+}
+
+onLoad();
